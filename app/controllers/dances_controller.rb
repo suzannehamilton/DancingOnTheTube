61d8d3696d1d07f84a51e586cbca0cd7d357d41d
Suzanne Hamilton <suzanne.m.hamilton@gmail.com>
2014-01-13 02:10:58 +0000

    Create page to show dance by ID

--- app/controllers/dances_controller.rb
+++ app/controllers/dances_controller.rb
@@ -8,6 +8,10 @@ class DancesController < ApplicationController
     redirect_to @dance
   end
 
+  def show
+    @dance = Dance.find(params[:id])
+  end
+
   private
     def post_params
       params.require(:dance).permit(:name)
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Dott::Application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
+    get 'dances/:id' => 'dances#show'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
