830ab3d9993458ce419aabe5cf46850bddd79d40
Suzanne Hamilton <suzanne.m.hamilton@gmail.com>
2014-01-13 00:01:35 +0000

    Initial commit of new dott rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dott::Application.config.secret_key_base = 'e9292d7f3f9bd4f852a3eb42f7ad4c50916eec6dd57b3c5e0ebc2c2dda9b8b6cdadb3bdc9711ef9edccebbb2351aace6901a233d9f18ad6f39d2f836f1990a24'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Dott::Application.config.session_store :cookie_store, key: '_dott_session'
