1d4b6ba6630a26dd4ac39574127fb15c82921aa9
Suzanne Hamilton <suzanne.m.hamilton@gmail.com>
2014-01-14 23:34:22 +0000

    Test dance index returns list of dances

--- test/controllers/dances_controller_test.rb
+++ test/controllers/dances_controller_test.rb
@@ -1,4 +1,13 @@
 require 'test_helper'
 
 class DancesControllerTest < ActionController::TestCase
+  test "index should list dances" do
+    get :index
+
+    listed_dances = assigns(:dances)
+
+    assert_response :success
+    assert listed_dances.include?(dances(:salsa))
+    assert_not_nil listed_dances
+  end
 end
