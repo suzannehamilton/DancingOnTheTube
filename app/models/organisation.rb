c8232abd4d057593946a61ca4e2edae41c90428b
Suzanne Hamilton <suzanne.m.hamilton@gmail.com>
2014-01-15 23:47:45 +0000

    Test presence of organisation URL

--- app/models/organisation.rb
+++ app/models/organisation.rb
@@ -2,4 +2,7 @@ class Organisation < ActiveRecord::Base
   validates :name,
             presence: true,
             :uniqueness => {:case_sensitive => false}
+
+  validates :url,
+            presence: true
 end
--- test/models/organisation_test.rb
+++ test/models/organisation_test.rb
@@ -1,32 +1,51 @@
 require 'test_helper'
 
 class OrganisationTest < ActiveSupport::TestCase
+  @@test_name = "new organisation"
+  @@test_url = "test url"
+
   test "should save valid organisation" do
     organisation = Organisation.new
-    organisation.name = "New organisation"
+    organisation.name = @@test_name
+    organisation.url = @@test_url
     assert organisation.save, "Did not save organisation"
   end
 
   test "organisation without name is invalid" do
     organisation = Organisation.new
+    organisation.url = @@test_url
+
     refute organisation.save, "Saved a organisation without a name"
   end
 
-  test "organisation with empty is invalid" do
+  test "organisation with empty name is invalid" do
     organisation = Organisation.new
     organisation.name = ""
+    organisation.url = @@test_url
+
     refute organisation.save, "Saved a organisation without a name"
   end
 
   test "organisation with duplicate name is invalid" do
     organisation = Organisation.new
     organisation.name = organisations(:salsa_org).name
+    organisation.url = @@test_url
+
     refute organisation.save, "Saved a organisation with duplicate name"
   end
 
   test "organisation with duplicate differently cased name is invalid" do
     organisation = Organisation.new
     organisation.name = organisations(:salsa_org).name.upcase
+    organisation.url = @@test_url
+
     refute organisation.save, "Saved a organisation with duplicate name"
+  end
+
+  test "organisation without url is invalid" do
+    organisation = Organisation.new
+    organisation.name = @@test_name
+
+    refute organisation.save, "Saved a organisation without a url"
   end
 end
