ac97b27ddefb944f551d892d9bad2bae751657d8
Suzanne Hamilton <suzanne.m.hamilton@gmail.com>
2014-01-22 22:42:40 +0000

    Make current page the active nav link

--- test/helpers/nav_helper_test.rb
+++ test/helpers/nav_helper_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class NavHelperTest < ActionView::TestCase
+  attr_reader :request
+
   test "generates nav tab" do
     assert_equal('<li><a href="/">Home</a></li>', nav_link('Home', root_path))
+  end
+
+  test "tab for current page is active" do
+    def self.current_page?(path)
+      true
+    end
+
+    assert_equal('<li class="active"><a href="/">Home</a></li>', nav_link('Home', root_path))
   end
 end
