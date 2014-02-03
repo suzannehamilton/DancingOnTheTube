35a26607b1a12dcbe2aafeba5e04e919f110c285
Suzanne Hamilton <suzanne.m.hamilton@gmail.com>
2014-02-03 22:57:40 +0000

    Add name to events

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140122232100) do
+ActiveRecord::Schema.define(version: 20140203225643) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20140122232100) do
     t.integer  "organisation_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
   add_index "events", ["organisation_id"], name: "index_events_on_organisation_id", using: :btree
