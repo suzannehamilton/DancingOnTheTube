0aae60e87e8dcfef42519ce07da53a541f2c5436
Suzanne Hamilton <suzanne.m.hamilton@gmail.com>
2014-02-06 21:00:01 +0000

    Add event create controller action

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,15 @@
 class EventsController < ApplicationController
   def new
   end
+
+  def create
+    @event = Event.new(post_params)
+    @event.save
+    redirect_to action: :index, controller: :organisations
+  end
+
+  private
+  def post_params
+    params.require(:event).permit(:name, :start_date)
+  end
 end
--- test/controllers/events_controller_test.rb
+++ test/controllers/events_controller_test.rb
@@ -5,4 +5,10 @@ class EventsControllerTest < ActionController::TestCase
     get :new
     assert_response :success
   end
+
+  test "create should create a new event" do
+    assert_difference('Event.count') do
+      post :create, event: {name: 'Test name', start_date: '2014-01-01'}
+    end
+  end
 end
