64e5de804dc1c326610b6afe68ef6a5e93d53c6d
Suzanne Hamilton <suzanne.m.hamilton@gmail.com>
2014-04-19 00:56:46 +0100

    Create a Location model, with a name, latitude and longitude

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140120205432) do
+ActiveRecord::Schema.define(version: 20140418235516) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,11 +27,40 @@ ActiveRecord::Schema.define(version: 20140120205432) do
     t.integer "organisation_id", null: false
   end
 
+  create_table "events", force: true do |t|
+    t.integer  "organisation_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "name"
+    t.date     "start_date"
+  end
+
+  add_index "events", ["organisation_id"], name: "index_events_on_organisation_id", using: :btree
+
+  create_table "locations", force: true do |t|
+    t.string   "name"
+    t.float    "latitude"
+    t.float    "longitude"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "organisations", force: true do |t|
     t.string   "name"
     t.string   "url"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "weekly_recurrences", force: true do |t|
+    t.integer  "event_id"
+    t.integer  "day_of_week"
+    t.integer  "frequency"
+    t.date     "end_date"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "weekly_recurrences", ["event_id"], name: "index_weekly_recurrences_on_event_id", using: :btree
 
 end
