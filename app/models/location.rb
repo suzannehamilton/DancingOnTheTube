5806d15e76de055a974c6814e602cf1425fa63e1
Suzanne Hamilton <suzanne.m.hamilton@gmail.com>
2014-04-19 01:05:57 +0100

    Make location latitude a required property

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,4 @@
 class Location < ActiveRecord::Base
-  validates :name,
-            presence: true
+  validates :name, presence: true
+  validates :latitude, presence: true
 end
--- test/models/location_test.rb
+++ test/models/location_test.rb
@@ -4,11 +4,18 @@ class LocationTest < ActiveSupport::TestCase
   test "should save valid location" do
     location = Location.new
     location.name = "New location"
+    location.latitude = 51.455209
     assert location.save, "Did not save new location"
   end
 
   test "location without a name is invalid" do
     location = Location.new
     refute location.save, "Saved a location without a name"
+  end
+
+  test "location without a latitude is invalid" do
+    location = Location.new
+    location.name = "New location"
+    refute location.save, "Saved a location without a latitude"
   end
 end
