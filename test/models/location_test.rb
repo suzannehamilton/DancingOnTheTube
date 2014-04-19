49ab89f87c490013ac246d2b9d6397ea418a33e1
Suzanne Hamilton <suzanne.m.hamilton@gmail.com>
2014-04-19 01:18:17 +0100

    Commonise attributes in location test

--- test/models/location_test.rb
+++ test/models/location_test.rb
@@ -2,31 +2,60 @@ require 'test_helper'
 
 class LocationTest < ActiveSupport::TestCase
   test "should save valid location" do
-    location = Location.new
-    location.name = "New location"
-    location.latitude = 51.455209
-    location.longitude = -0.037112
+    location = LocationBuilder.new
+      .withName
+      .withLatitude
+      .withLongitude
+      .build
     assert location.save, "Did not save new location"
   end
 
   test "location without a name is invalid" do
-    location = Location.new
-    location.latitude = 51.455209
-    location.longitude = -0.037112
+    location = LocationBuilder.new
+      .withLatitude
+      .withLongitude
+      .build
     refute location.save, "Saved a location without a name"
   end
 
   test "location without a latitude is invalid" do
-    location = Location.new
-    location.name = "New location"
-    location.longitude = -0.037112
+    location = LocationBuilder.new
+      .withName
+      .withLongitude
+      .build
     refute location.save, "Saved a location without a latitude"
   end
 
   test "location without a longitude is invalid" do
-    location = Location.new
-    location.name = "New location"
-    location.latitude = 51.455209
+    location = LocationBuilder.new
+      .withName
+      .withLatitude
+      .build
     refute location.save, "Saved a location without a latitude"
+  end
+end
+
+class LocationBuilder
+  def initialize
+    @location = Location.new
+  end
+
+  def withName
+    @location.name = "Some name"
+    self
+  end
+
+  def withLatitude
+    @location.latitude = 51.455209
+    self
+  end
+
+  def withLongitude
+    @location.longitude = -0.037112
+    self
+  end
+
+  def build
+    @location
   end
 end
