b95dc62ca90778fc36c86e5243a484c7c80bc382
Suzanne Hamilton <suzanne.m.hamilton@gmail.com>
2014-04-20 22:19:10 +0100

    Test that an event's weekly recurrence can be saved

--- test/models/weekly_recurrence_test.rb
+++ test/models/weekly_recurrence_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class WeeklyRecurrenceTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "a valid event can be saved" do
+    weekly_recurrence = WeeklyRecurrence.new
+    weekly_recurrence.day_of_week = 0
+    weekly_recurrence.frequency = 1
+    weekly_recurrence.event = events(:salsa_event)
+
+    assert weekly_recurrence.save, "Did not save a valid recurrence"
+  end
 end
