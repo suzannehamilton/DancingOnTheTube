8f57acbd656fa43498489dbdd3919c5db00270f1
Suzanne Hamilton <suzanne.m.hamilton@gmail.com>
2014-04-20 15:01:25 +0100

    Link organisations to locations

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,6 @@
 class Location < ActiveRecord::Base
+  has_and_belongs_to_many :organisations
+
   validates :name, presence: true
   validates :latitude, presence: true
   validates :longitude, presence: true
