2e0ebc099c0b2f4959557ae3e6475402e6e72c07
Suzanne Hamilton <suzanne.m.hamilton@gmail.com>
2014-09-17 20:57:44 +0100

    Fix nav highlight bug - highlight navigation tab when current page is any child of that tab

--- app/helpers/nav_helper.rb
+++ app/helpers/nav_helper.rb
@@ -1,6 +1,6 @@
 module NavHelper
   def nav_link link_text, link_path
-    class_name = current_page?(link_path) ? 'active' : nil
+    class_name = request.fullpath.starts_with?(link_path) ? 'active' : nil
 
     content_tag(:li,  :class => class_name) do
       link_to link_text, link_path
